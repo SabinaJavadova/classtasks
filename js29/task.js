2ea5ec16c3edb5960af3b925dc63397874cf3dc0
Shahnazrustamli <shahnazrtm@gmail.com>
2024-10-29 18:01:43 +0400

    29.10

---
+++ js29/task.js
@@ -0,0 +1,210 @@
+      // task1
+
+// let age = prompt("enter age: ");
+
+// if(age>=0){
+//   if(age>=65){
+//     alert("pensiya yashindasiz.");
+    
+//   }
+//   else{
+//     alert("pensiyaya aid deil yasiniz")
+//   }
+// }
+// else{
+//   alert("duzgun yas daxil et")
+// }
+
+        // task2
+
+        // let teref1= prompt("birinci teref:");
+        // let teref2= prompt("ikinci teref:");
+        // let teref3= prompt("ucuncu teref:");
+
+        // if(teref1==teref2==teref3){
+        //   alert("beraberterefli")
+        // }else if (teref1==teref2!=teref3){
+        //   alert("beraberyanli")
+        // }else if(teref1!=teref2!=teref3){
+        //   alert("muxtelifterefli")
+        // }else{
+        //   alert("bele fiqur yoxdur")
+        // }
+
+        // task3
+
+        // let eded= prompt("ededi yaz:");
+        // if (eded%2==0){
+        //   alert("cut eded")
+        // }else {
+        //   alert("eded tekdir.")
+        // }
+
+
+        // task4
+      
+        // let ay = +prompt("ay daxil et");
+
+        // switch (ay){
+        //   case 1:
+        // alert("yanvar");
+        // break;
+        // case 2:
+        //   alert("fevral");
+        //   break;
+        //   case 3:
+        //     alert("mart");
+        //     break;
+        //     case 4:
+        //       alert("aprel");
+        //       break;
+        //       case 5:
+        //         alert("may");
+        //         break;
+        //         case 6:
+        //           alert("iyun");
+        //           break;
+        //           case 7:
+        //             alert("iyul");
+        //             break;
+        //             case 8:
+        //               alert("avqust");
+        //               break;
+        //               case 9:
+        //                 alert("sentyabr");
+        //                 break;
+        //                 case 10:
+        //                   alert("oktyabr");
+        //                   break;
+        //                   case 11:
+        //                     alert("noyabr");
+        //                     break;
+        //                     case 12:
+        //                       alert("dekabr");
+        //                       break;
+        //   default:alert("duzgun deyil")
+
+        // }
+
+        // task5
+
+        // let eded = prompt("eded daxilet: ");
+        // if (eded%3==0 && eded%5==0 ){
+        //   alert("3 ve 5e bolunur");
+        // }else{
+        //   alert("bolunmur");
+        // }
+
+        // task6
+
+        // let eded = prompt("ededi daxil et");
+        // if(eded%2==0){
+        //   alert("2e tam bolunur");
+        // }else {
+        //  alert (eded%2);
+        // }
+
+        // task7
+        // let eded1 = prompt("birinci ededi yaz:");
+        // let eded2 = prompt("ikinci ededi yazin:");
+
+        // if(eded1*eded2>0){
+        //   alert("hasil musbetdir");
+        // }else {
+        //   alert("hasil menfidir");
+        // }
+
+        // let eded = prompt("ededi daxil et");
+
+        // if (eded>0 && 1<eded<100 ){
+        //   alert("eded musbetdir ve 1le 100 arasindadir");
+        // }else{
+        //   alert("arasinda deyil");
+        // }
+
+        // task9
+      //    let eded1 = prompt("birinci ededi yaz:");
+      //  let eded2 = prompt("ikinci ededi yazin:");
+      //  let eded3 = prompt("ucuncu ededi yazin:");
+
+      //  if (eded1>eded2  && eded1>eded3 ){
+      //   alert("birinci en boyuyudur");
+      //  }else if(eded2>eded1 && eded2>eded3 && eded2>eded3){
+      //   alert("ikincui boyukdu")
+      //  }else if(eded3>eded1 && eded3>eded2 ){
+      //   alert("ucuncu boyukdu")
+      //  }
+      //  else{
+      //   alert("bele bir eded yoxdur");
+      //  }
+        // task10
+      // let bal = prompt("bali daxil et: ");
+      // if(bal>90 && 100>bal>=0){
+      //   alert("A")
+      // }else if(80<bal<89  && 100>bal>=0){
+      //   alert("B")
+      // }else if(70<bal<79  && 100>bal>=0){
+      //   alert("C")
+      // }else if(60<bal<69  && 100>bal>=0){
+      //   alert("D")
+      // }else if(50<bal<59  && 100>bal>=0) {
+      //   alert("E")
+      // }else if(40<bal<49  && 100>bal>=0){
+      //   alert("F")
+      // }else{
+      //   alert("bele bal yoxdur");
+      // }
+
+      // task11
+      // let yash = prompt("yashi daxil et");
+      // if(yash<18){
+      //   alert("yeniyetme");
+      // }else if (18<yash<64){
+      //   alert("yetkin");
+      // }else if (65<yash){
+      //   alert("Yashli");
+      // }else{
+      //   alert("bele yash sistemi yoxdur.")
+      // }
+
+      // task12
+      // let saat = prompt("saati daxil et")
+      // if (saat<12){
+      //   alert("sabahiniz xeyr");
+      // }else if(12<saat<16){
+      //   alert("gunortniz xeyr");
+      // }else if(16<saat<23){
+      //   alert("axsaminiz xeyr");
+      // }else{
+      //   alert("bele saat yoxdu");
+      // }
+
+      // task13
+      //       let x = prompt("x:");
+      // let y = prompt("y:");
+      // if( x>0 && y<0){
+      //       alert( 4*x+2*y+4 );
+      // }else if (x>0 && y==0 ){
+      //       alert( 2*x-y+3 );
+      // }else if (x<0 && y>0 ){
+      //       alert( 3*x+4*y+3 );
+      // }else{
+      //       alert("hesablama sehvdir");
+      // }
+
+      // task14
+      // let eded1 = prompt("eded1:");
+      // let eded2 = prompt("eded2:");
+      // let eded3 = prompt("eded3:");
+
+      // sum = eded1+eded2+eded3
+      // alert(sum/3)
+
+      // task15
+      // ad = prompt("your name: ");
+      // soyad = prompt("your SurName: ");
+      // Fullname = prompt(ad +" " +soyad);
+           
+      // alert(Fullname)
+
+      
