c6bb0747aa96f6641c76b564d0afd581c82b154a
Sabina Javadova <152090079+SabinaJavadova@users.noreply.github.com>
2024-10-30 19:28:08 +0400

    30.10

---
+++ js30/task.js
@@ -0,0 +1,179 @@
+// for (let index = 0; index <= 100; index++) {
+// if(index%2==0){
+// console.log(index);
+
+// }
+  
+// }
+// let count =0;
+// for (let index = 0; index <= 100; index++) {
+//   if(index%2==1){
+//   count++;
+  
+//   }
+  
+// }
+// console.log(count);
+
+//  let summ=0
+//   for (let index = 0; index < 100; index++) {
+//     if(index%5==0){
+//     console.log(index);
+//     summ += index
+//     }
+      
+//     }
+//     console.log(summ);
+    
+
+let array =[12,14,35,56,68,93]
+
+  // task1
+// let sum=0;
+// for (let i = 0; i < array.length; i++) {
+  
+//   sum+=array[i]
+// }
+// console.log(sum);
+// task2
+// for (let i = 0; i < array.length; i=i+2) {
+ 
+//   console.log(array[i]);
+  
+// }
+
+// task3
+
+// for (let i = 1; i < array.length; i=i+2) {
+ 
+//   console.log(array[i]);
+  
+// }
+
+// task4
+
+// let largest = array[0];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i]>largest) {
+//     largest=array[i]
+//   }
+  
+// }
+// console.log(largest);
+
+
+// task5
+
+// let largest = array[0];
+// let lowest = array[0];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i]>largest){
+//     largest=array[i]
+//   }
+  
+// }
+// for (let i = 0; i < array.length; i++) {
+//   if(array[i]<lowest)
+//   lowest=array[i]
+// }
+
+// console.log(largest-lowest);
+
+
+// task6
+
+// let eded = prompt("ededi yaz: ");
+// if (eded == array[i]) {
+//   alert(`${array[i]} arreye daxildir`);
+// }
+
+// task7
+
+// const employees = [
+//   { name: "Jamil", salary: 50000, department: "IT" },
+//   { name: "Jale", salary: 60000, department: "HR" },
+//   { name: "Bayram", salary: 55000, department: "IT" },
+//   { name: "Sahil", salary: 75000, department: "HR" },
+//   { name: "Maryam", salary: 65000, department: "IT" },
+//   { name: "Elnara", salary: 80000, department: "HR" },
+//   { name: "Davud", salary: 70000, department: "IT" },
+// ];
+
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].salary>60000) {
+//     console.log(employees[i].name);
+     
+//   }
+  
+// }
+
+// let eded = +prompt("ededi daxil et: ")
+// let fakt= 1;
+
+// for (let i = 1; i <= eded; i++) {
+//   fakt=fakt*i
+  
+// }
+
+// alert(fakt);  
+
+
+// let str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, laborum.";
+
+// let count = 0;
+// for (let i = 0; i < str.length; i++) {
+//  if(str[i]="l"){
+//   count ++
+//  }
+  
+// } 
+// console.log(count);
+
+// const country= ["azerbaijan","norwey","albania","germany","america"];
+// for (let i = 0; i < country.length; i++) {
+//   if(country[i][0]=="a" && country[i][country[i].length-1]=="a"){
+//     console.log(country[i]);
+    
+//   }
+  
+// }
+
+// let eded = 427;
+
+// while (eded > 0) {
+//  let  reminder = number % 10;
+//  console.log(reminder);
+//  number = (number-reminder) /10;
+// }
+
+
+// let eded =5;
+// let isprime = true;
+// for (let i = 2; i < eded; i++) {
+  
+//   if (eded % i == 0){
+//    isprime =false;
+//    break;
+    
+//   }
+// }
+// console.log(isprime);
+
+
+const scores =[60,85,88,75,93,45];
+// for (let i = 0; i < array.length; i++) {
+  
+//   sum +=scores;
+  
+// }
+// console.log(sum/scores.length);
+
+
+//  largest =0;
+// for (let i = 0; i < scores.length; i++) {
+//   if (scores[i]>scores[i-1]) {
+//     largest=scores[i]
+//   }
+  
+// }
+// console.log(largest);
