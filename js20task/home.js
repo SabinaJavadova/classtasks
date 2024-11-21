9ee4f0d8023ada04d0928d7d8cf61cbeb356f016
ClassTime <classtime@code.local>
2024-11-21 18:33:55 +0400

    21.11

--- js20task/home.js
+++ js20task/home.js
@@ -15,17 +15,26 @@ function drawCards(arr) {
                 <div class="card-body">
                 <h3 class="cardName">${car.name}</h3>
                 <p class="cardType">${car.type}</p>
-                <img src="${car.Image}" class="carImg" alt="${car.name}" width= "100%">
-                        <p class="fuel"><i class="fa-solid fa-gas-pump"></i> ${car.fuel}</p>
-                        <p class="transmission"><i class="fa-solid fa-circle-notch"></i> ${car.transmission}</p>
-                        <p class="passengers"><i class="fa-solid fa-users-line"></i> ${car.passengers}</p>
+                   <div class="image">
+
+                   <img src="${car.Image}" class="carImg" alt="${car.name}" width= "100%">
+                                    
+                                </div>
+                                <div class="divs">
+                                <div class="ikons">
+  
+                                <p class="fuel"><i class="fa-solid fa-gas-pump"></i> ${car.fuel}</p>
+                                <p class="transmission"><i class="fa-solid fa-circle-notch"></i> ${car.transmission}</p>
+                                <p class="passengers"><i class="fa-solid fa-users-line"></i> ${car.passengers}</p>
+       </div>
                         <span class="car-price">Price: $${car.price}</span>
                         <button class="addToBasket">Add to Basket</button>
                     </div>
+                                </div>
                 </div>
             </div>
         `;
-  });document.querySelectorAll(".addToBasket").forEach((btn) => {
+  }); document.querySelectorAll(".addToBasket").forEach((btn) => {
     btn.addEventListener("click", (event) => {
       const id = event.target.getAttribute("data-id");
       addToBasket(id);
