37aa4b9d06e9d3798672bca0043fd9a18be47c11
Sabina Javadova <sabinacavadova005@gmail.com>
2024-11-23 02:32:16 +0400

    basket wishlist half

--- js20task/baskets.js
+++ js20task/baskets.js
@@ -1,9 +1,33 @@
-import { products } from "./data.js";
+import { carsData } from "./data.js";
 
 import {
-    calcBasketCount,
-    getDataFromLocalStorage,
-    setDataToLocalStorage,
-    showUserInfo,
-  } from "./helpers.js";
-  
+  getFromLocalstore,
+  setToLocalstorage,
+} from "./allFunc.js";
+
+const tBody = document.querySelector("tbody");
+const clearBtn = document.querySelector("clear-all");
+const totalPrice = document.querySelector("total-price");
+const users = JSON.parse(localStorage.getItem('users')) || [];  // Initialize `users` from localStorage
+
+const loggedInUser = users.find((us) => us.isLogged);
+
+// console.log(loggedInUser);
+
+
+function drawTable(basketArr, carsDataArr) {
+  tBody.innerHTML = "";
+  basketArr.forEach((item) => {
+    console.log(item);
+    const carsData = carsDataArr.find((p) => p.id == item.carsDataId);
+
+    const trElem = document.createElement("tr");
+    trElem.innerHTML = `
+        <td><img src="${carsData.Image}" width="100"/></td>
+                      <td>${carsData.type}</td>
+                      <td>$ ${carsData.price}</td>
+                      
+    `;
+    tBody.append(trElem);
+  });
+}
