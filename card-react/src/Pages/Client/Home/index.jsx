08fe1bfd64f3c9ddfa4120f962d0b605746d4473
Sabina Javadova <sabinacavadova005@gmail.com>
2024-12-28 01:51:28 +0400

    favorites done

--- card-react/src/Pages/Client/Home/index.jsx
+++ card-react/src/Pages/Client/Home/index.jsx
@@ -1,73 +1,50 @@
-import React from 'react'
-import { endpoints } from '../../../Data/constant.js';
-import { useState, useEffect } from 'react';
-import getAllData from "../../../Data/index.js"
-import "../Home/index.css"
+import React, { useState, useEffect, useContext } from 'react';
+import { FavoritesContext } from '../../../Context/FavoritesContext';
 import { MdFavorite } from "react-icons/md";
+import getAllData from "../../../Data/index.js";
+import { endpoints } from "../../../Data/constant.js";
+import "../Home/index.css";
+
 const Home = () => {
-
   const [products, setProducts] = useState([]);
-  
-const getProducts = async () => {
-  
-  try {
-    const data = await getAllData(endpoints.products);
-    console.log(data);
+  const { toggleFavorites } = useContext(FavoritesContext); 
 
-    setProducts(data); 
-        
-  } catch (error) {
-    console.log(error);
-  }
-};
-useEffect(() => {
-  getProducts();
+  const getProducts = async () => {
+    try {
+      const data = await getAllData(endpoints.products);
+      console.log(data);
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-}, []);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
-    <>
     <div className="App">
       <div className="cards">
-
-      {products.length> 0 && products.map((p)=>{
-        return(
+        {products.length > 0 &&
+          products.map((p) => (
             <div className="product-card" key={p.id}>
-            {}
-            <div className="ikon">
-
-            <MdFavorite />
+              <div className="ikon" onClick={() => toggleFavorites(p)}>
+                <MdFavorite />
+              </div>
+              <div className="product-image">
+                <img src={p.image} alt={p.title} />
+              </div>
+              <div className="product-details">
+                <h3 className="product-title">{p.title}</h3>
+                <p className="product-description">{p.description.slice(0, 40)}...</p>
+                <p className="product-price">${p.price}</p>
+              </div>
             </div>
-            <div className="product-image">
-              <img src={p.image} alt={p.title} />
-            </div>
-      
-            {}
-            <div className="product-details">
-              <h3 className="product-title">{p.title}</h3>
-              <p className="product-description">{p.description.slice(0,40)} </p>
-              <p className="product-price">${p.price}</p>
-            </div>
-      
-            {}
-          </div>
-      
-        )
-      })}
+          ))}
       </div>
-
-      {}
-    
-
-      {}
-      <footer className="footer">
-        <div className="container">
-          <p className="footer-text">&copy; 2024 Your Company. All rights reserved.</p>
-        </div>
-      </footer>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- js22api/script.js
+++ js22api/script.js
@@ -52,7 +52,7 @@ function drawTable(arr) {
                     <td>${item.address?.phone}</td>
                     <td class="d-flex gap-2">
                 <button class="delete btn btn-outline-danger" data-id=${item.id}>delete</button>
-                <button class="edit btn btn-outline-success">edit</button>
+                <button class="edit btn btn-outline-success" >edit</button>
                 <a class="btn btn-outline-primary d-flex align-items-center" href="details.html?id=${item.id}">details</a>
             </td>
                   </tr>
